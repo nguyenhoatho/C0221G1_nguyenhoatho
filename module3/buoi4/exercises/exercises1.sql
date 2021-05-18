7e64b787cb7b7f6b2930d85cb9f0e979e86e7e29
nguyenhoatho <nguyenhoatho.qn@gmail.com>
2021-05-18 10:33:40 +0700

    fix loi bai tap

--- module3/buoi4/exercises/exercises1.sql
+++ module3/buoi4/exercises/exercises1.sql
@@ -1,17 +1,30 @@
-use student_management;
+use quan_ly_sinh_vien;
 
 -- Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất.
-select sb.sub_id, sb.sub_name,max(sb.credit)as max_credit,sb.`Status`
-from `subject` as sb;
+select *,max(sub.credit)
+from `subject` sub
+group by sub.sub_id
+having max(sub.credit) >= all (
+		select max(sub.credit)
+		from `subject` sub
+		group by sub.sub_id)
+;
 
--- Hiển thị các thông tin môn học có điểm thi lớn nhất
-select sb.sub_id ,sb.sub_name,sb.credit,sb.`status`,max(mark.mark)as max_mark
-from `subject` as sb
-join mark on sb.sub_id=mark.sub_id;
+-- Hiển thị các thông tin môn học có điểm thi lớn nhất.
+select sub.sub_id,sub.sub_name,max(m.mark)
+from `subject` sub
+join mark m on sub.sub_id = m.sub_id
+group by m.sub_id
+having max(m.mark) >= all (
+		select max(m.mark)
+		from `subject` sub
+		join mark m on sub.sub_id = m.sub_id
+		group by m.sub_id)
+;
 
 -- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
-select student.student_id,student.student_name,student.address,student.Phone,student.class_id, avg(mark.mark) as "diem trung binh"
-from student
-join mark on student.student_id=mark.student_id
-group by student.student_id
-order by avg(mark.mark) desc;
+select s.student_id,s.student_name,avg(m.mark)
+from student s
+left join mark m on m.student_id = s.student_id
+group by m.student_id
+order by m.student_id desc,s.student_name;
--- module3/casestudy/6.task16-18/task16-18.sql
+++ module3/casestudy/6.task16-18/task16-18.sql
@@ -9,3 +9,21 @@ where nhan_vien.id_nhan_vien  not in
 	inner join hop_dong on nhan_vien.id_nhan_vien=hop_dong.id_nhan_vien
 	where year( hop_dong.ngay_hop_dong) in (2017,2018,2019)
     ) as tmp);
+    
+-- 17.	Cập nhật thông tin những khách hàng có TenLoaiKhachHang từ  Platinium lên Diamond, chỉ cập nhật những khách
+--  hàng đã từng đặt phòng với tổng Tiền thanh toán trong năm 2019
+--  là lớn hơn 10.000.000 VNĐ.
+
+update khach_hang
+set khach_hang.id_loai_khach = 5
+where khach_hang.id_khach_hang in
+( select kh.id_kh from(select khach_hang.id_khach_hang as id_kh
+	from khach_hang 
+    join hop_dong on hop_dong.id_khach_hang=khach_hang.id_khach_hang
+    where khach_hang.id_loai_khach = 4
+    group by khach_hang.id_loai_khach,hop_dong.id_khach_hang
+    having sum(hop_dong.tong_tien) >10000000
+    )as kh
+ );
+ 
+ 
